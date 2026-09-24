f91232dd24dbebaa14599c34bcae5decf1d9acf3
mohammad.en <mohammad.en1381@gmail.com>
2026-09-24 16:47:38 +0330

    get port from env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -88,7 +88,7 @@ Production-ready NestJS Backend with:
     customSiteTitle: 'University Internship API Docs',
   });
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT;
   await app.listen(port, '0.0.0.0');
 
   logger.log(`=======================================================`);
